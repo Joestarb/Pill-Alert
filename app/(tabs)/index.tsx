78e1298a4baf97b7fa13036f6825a3ecac1a52eb
NierRep24 <danielsanchezmartinez064@gmail.com>
2025-05-26 12:24:49 -0500

    Añadido componente de notificacion, un boton de muestra para ver la notificacion, imagen hecha en figma para la notificacion

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,21 +2,30 @@
 import { Redirect } from "expo-router";
 import { useSelector } from "react-redux";
 import type { RootState } from "../../contexts/store";
-import { Text, View } from "react-native";
+import { Text, View , Button} from "react-native";
+import NotiModal from "@/components/NotiModal";
+import React, { useState } from 'react';
 
 export default function HomeScreen() {
   const isAuthenticated = useSelector(
     (state: RootState) => state.auth.isAuthenticated
   );
 
-  if (!isAuthenticated) {
-    return <Redirect href="/login" />;
-  }
+  const [showModal, setShowModal] = useState(false);
+
+  // if (!isAuthenticated) {
+  //   return <Redirect href="/login" />;
+  // }
 
   // Aquí puedes renderizar el contenido principal de la pantalla Home
   return (
     <View>
       <Text>index</Text>
+      <Button title="Mostrar notificación" onPress={() => setShowModal(true)} />
+      <NotiModal
+      visible={showModal}
+      onClose={() => setShowModal(false)}
+      />
     </View>
   );
 }
